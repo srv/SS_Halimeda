6095d0252654be9bac70c4da693b8d75a2f83d0e
Lpopvil <113474413+Lpopvil@users.noreply.github.com>
2022-09-13 22:38:50 +0200

    Add files via upload

---
+++ Github/CNNhalimeda.py
@@ -0,0 +1,81 @@
+import tensorflow 
+import os
+import numpy as np
+import cv2
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import load_img
+
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+
+import matplotlib.pyplot as plt
+
+
+DG_folder='C:/Users/person/Documents/FDR_Images/DA_Images/halimeda_train/images'
+DG_folder1='C:/Users/person/Documents/FDR_Images/DA_Images/halimeda_train/masks'
+images_increased = 5
+
+try:
+    os.mkdir(DG_folder)
+    os.mkdir(DG_folder1)
+except:
+    print("")
+
+
+train_datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='reflect')
+
+
+data_path = "C:/Users/person/Documents/FDR_Images/halimeda_train/images" 
+mask_path = "C:/Users/person/Documents/FDR_Images/halimeda_train/masks" 
+
+data_dir_list = os.listdir(data_path)
+
+
+width_shape, height_shape = 1024,1024
+
+i=0
+num_images=0
+for image_file in data_dir_list:
+    img_list=os.listdir(data_path)
+    print(image_file)
+
+    img_path = data_path + '/'+ image_file
+    mask_file = mask_path + '/gt_'+image_file
+    
+    imge=load_img(img_path)
+    mask=load_img(mask_file)
+    
+    imge=cv2.resize(tensorflow.keras.utils.img_to_array(imge), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
+    mask=cv2.resize(tensorflow.keras.utils.img_to_array(mask), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
+
+    x= imge/255
+    y= mask/255
+
+    x=np.expand_dims(x,axis=0)
+    y=np.expand_dims(y,axis=0)
+    t=1
+    for output_batch_img, output_batch_mask in zip(train_datagen.flow(x,batch_size=1), train_datagen.flow(y,batch_size=1)):
+        a=tensorflow.keras.utils.img_to_array(output_batch_img[0])
+        imagen=output_batch_img[0,:,:]*255
+        imgfinal = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(DG_folder+"/%i%i.jpg"%(i,t), imgfinal)
+        
+        b=tensorflow.keras.utils.img_to_array(output_batch_mask[0])
+        imagen1=output_batch_mask[0,:,:]*255
+        imgfinal1 = cv2.cvtColor(imagen1, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(DG_folder1+"/gt_%i%i.jpg"%(i,t), imgfinal1)
+        t+=1
+        
+        num_images+=1
+        if t>images_increased:
+            break
+    i+=1
+    
+print("images generated",num_images)
