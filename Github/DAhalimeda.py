9d240fd27a6398db9ff03c2e0317f4a4c9680b85
Lpopvil <113474413+Lpopvil@users.noreply.github.com>
2022-10-15 21:17:46 +0200

    Update DAhalimeda.py

--- Github/DAhalimeda.py
+++ Github/DAhalimeda.py
@@ -1,80 +1,80 @@
-import numpy as np
-import tensorflow as tf
+import tensorflow 
 import os
-import random
-import cv2 #pip install opencv-python
- 
-from tqdm import tqdm 
-
-from skimage.io import imread, imshow
-from skimage.transform import resize
-import matplotlib.pyplot as plt
-
-import seaborn as sns
+import numpy as np
 import cv2
-  
-# read the image file
-img1 = cv2.imread(r"C:\Users\person\FDR_Images\IMGsCM\Case3\img1.png",2)
-img2 = cv2.imread(r"C:\Users\person\FDR_Images\IMGsCM\Case3\img2.png",2)
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import load_img
 
-ret, bw_img1 = cv2.threshold(img1, 127, 255, cv2.THRESH_BINARY)
-ret, bw_img2 = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY)
 
-cv2.imshow("Binary1", bw_img1)
-cv2.imshow("Binary2", bw_img2)
 
-rows1,cols1 = bw_img1.shape
-rows2,cols2 = bw_img2.shape
 
-confMatrix=np.zeros((2,2))
 
-for i in range(rows1):
-    for j in range(cols1):
-       if bw_img1[i,j]==0:
-           if bw_img2[i,j]==0:
-               confMatrix[1,1]=confMatrix[1,1]+1
-           else:
-               confMatrix[0,1]=confMatrix[0,1]+1
-       else:
-           if bw_img2[i,j]==0:
-               confMatrix[1,0]=confMatrix[1,0]+1
-           else:
-               confMatrix[0,0]=confMatrix[0,0]+1
-               
-print(confMatrix)
-                  
-ax = sns.heatmap(confMatrix, annot=True, cmap='Blues', fmt='g')
 
-ax.set_title('Seaborn Confusion Matrix with labels\n\n');
-ax.set_xlabel('\nActual Values')
-ax.set_ylabel('Predicted Values ');
+DG_folder='C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/DA_NotDetailedIMGs/halimeda_train/images'
+DG_folder1='C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/DA_NotDetailedIMGs/halimeda_train/masks'
+images_increased = 5
 
-## Ticket labels - List must be in alphabetical order
-ax.xaxis.set_ticklabels(['True','False'])
-ax.yaxis.set_ticklabels(['True','False'])
+try:
+    os.mkdir(DG_folder)
+    os.mkdir(DG_folder1)
+except:
+    print("")
 
-## Display the visualization of the Confusion Matrix.
-plt.show()
+
+train_datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='reflect')
+
+
+data_path = "C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/NotDetailedIMGs/halimeda_train/images" 
+mask_path = "C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/NotDetailedIMGs/halimeda_train/masks" 
+
+data_dir_list = os.listdir(data_path)
+
+
+width_shape, height_shape = 1024,1024
+
+i=0
+num_images=0
+for image_file in data_dir_list:
+    img_list=os.listdir(data_path)
+    print(image_file)
+
+    img_path = data_path + '/'+ image_file
+    mask_file = mask_path + '/gt_'+image_file
     
-TP=confMatrix[0,0]
-TN=confMatrix[1,1]
-FP=confMatrix[1,0]
-FN=confMatrix[0,1]
-           
-Precision = TP / (FP + TP)
-Recall = TP / (TP + FN)
-F1score = 2*(Recall*Precision)/(Recall + Precision)
-Accuracy = (TP + TN)/(TP + FP + TN + FN)
+    imge=load_img(img_path)
+    mask=load_img(mask_file)
+    
+    imge=cv2.resize(tensorflow.keras.utils.img_to_array(imge), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
+    mask=cv2.resize(tensorflow.keras.utils.img_to_array(mask), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
 
-print('Precision',Precision)
-print('Recall',Recall)
-print('F1score',F1score)
-print('Accuracy',Accuracy)
-print('TP:',TP)
-print('TN:',TN)
-print('FP:',FP)
-print('FN:',FN)
+    x= imge/255
+    y= mask/255
 
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    x=np.expand_dims(x,axis=0)
+    y=np.expand_dims(y,axis=0)
+    t=1
+    for output_batch_img, output_batch_mask in zip(train_datagen.flow(x,batch_size=1), train_datagen.flow(y,batch_size=1)):
+        a=tensorflow.keras.utils.img_to_array(output_batch_img[0])
+        imagen=output_batch_img[0,:,:]*255
+        imgfinal = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(DG_folder+"/%i%i.jpg"%(i,t), imgfinal)
+        
+        b=tensorflow.keras.utils.img_to_array(output_batch_mask[0])
+        imagen1=output_batch_mask[0,:,:]*255
+        imgfinal1 = cv2.cvtColor(imagen1, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(DG_folder1+"/gt_%i%i.jpg"%(i,t), imgfinal1)
+        t+=1
+        
+        num_images+=1
+        if t>images_increased:
+            break
+    i+=1
+    
+print("images generated",num_images)
