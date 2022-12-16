b6d91ce620298fcd7a52357336a3e120412e455f
mmabadal <mmabadal@gmail.com>
2022-12-16 15:58:32 +0100

    fix and clean data augmentation

--- scripts/DAhalimeda.py
+++ scripts/DAhalimeda.py
@@ -1,76 +1,52 @@
-import tensorflow 
 import os
-import numpy as np
 import cv2
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.utils import load_img
+import random
+import tensorflow 
+import numpy as np
+from tensorflow import keras 
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import save_img
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+path_out = "/home/uib/Desktop/SS_Halimeda/Halimeda_Images/test_da/out/"
+path_in = "/home/uib/Desktop/SS_Halimeda/Halimeda_Images/test_da/in/"
 
-DG_folder='C:/Users/person/Documents/FDR_Images/DA_Images/halimeda_train/images'
-DG_folder1='C:/Users/person/Documents/FDR_Images/DA_Images/halimeda_train/masks'
-images_increased = 5
+path_out_img= os.path.join(path_out + "img")
+path_out_mask= os.path.join(path_out + "mask")
 
 try:
-    os.mkdir(DG_folder)
-    os.mkdir(DG_folder1)
+    os.mkdir(path_out_img)
+    os.mkdir(path_out_mask)
 except:
     print("")
 
+path_in_img=  os.path.join(path_in + "img")
+path_in_mask= os.path.join(path_in + "mask")
 
-train_datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='reflect')
+for image_file in os.listdir(path_in_img):
 
+    s = random.randint(1, 20000)
 
-data_path = "C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/NotDetailedIMGs/halimeda_train/images" 
-mask_path = "C:/Users/tomas/Documents/TFG/TFG_Images/IMGs_SelectionOfImages/SubSelection_GoodFor_Segmentation/NotDetailedIMGs/halimeda_train/masks" 
-
-data_dir_list = os.listdir(data_path)
-
-
-width_shape, height_shape = 1024,1024
-
-i=0
-num_images=0
-for image_file in data_dir_list:
-    img_list=os.listdir(data_path)
-    print(image_file)
-
-    img_path = data_path + '/'+ image_file
-    mask_file = mask_path + '/gt_'+image_file
+    img_path = os.path.join(path_in_img, image_file)
+    mask_file = os.path.join(path_in_mask,"gt_" + image_file)
     
-    imge=load_img(img_path)
+    image=load_img(img_path)
     mask=load_img(mask_file)
     
-    imge=cv2.resize(tensorflow.keras.utils.img_to_array(imge), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
-    mask=cv2.resize(tensorflow.keras.utils.img_to_array(mask), (width_shape, height_shape), interpolation = cv2.INTER_AREA)
+    image=np.expand_dims(image,axis=0)
+    mask=np.expand_dims(mask,axis=0)
 
-    x= imge/255
-    y= mask/255
+    #datagen = ImageDataGenerator(rotation_range=20,width_shift_range=0.1,height_shift_range=0.1,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode='reflect')
+    datagen = ImageDataGenerator(rotation_range=50,width_shift_range=0.5,height_shift_range=0.5,shear_range=0.5,zoom_range=0.5,horizontal_flip=True,fill_mode='reflect')
+        
+    aug_iter_img = datagen.flow(image, batch_size=1, seed=s)
+    image_aug = next(aug_iter_img)[0].astype('uint8')
 
-    x=np.expand_dims(x,axis=0)
-    y=np.expand_dims(y,axis=0)
-    t=1
-    for output_batch_img, output_batch_mask in zip(train_datagen.flow(x,batch_size=1), train_datagen.flow(y,batch_size=1)):
-        a=tensorflow.keras.utils.img_to_array(output_batch_img[0])
-        imagen=output_batch_img[0,:,:]*255
-        imgfinal = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(DG_folder+"/%i%i.jpg"%(i,t), imgfinal)
-        
-        b=tensorflow.keras.utils.img_to_array(output_batch_mask[0])
-        imagen1=output_batch_mask[0,:,:]*255
-        imgfinal1 = cv2.cvtColor(imagen1, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(DG_folder1+"/gt_%i%i.jpg"%(i,t), imgfinal1)
-        t+=1
-        
-        num_images+=1
-        if t>images_increased:
-            break
-    i+=1
-    
-print("images generated",num_images)
+    aug_iter_mask = datagen.flow(mask, batch_size=1, seed=s)
+    mask_aug = next(aug_iter_mask)[0].astype('uint8')
+
+    save_img(os.path.join(path_out_img,image_file),image_aug)
+    save_img(os.path.join(path_out_mask, "gt_" + image_file),mask_aug)
+
+
+
