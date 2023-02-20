16b6d8d13218a1e5d02a0567c7e5765afd816ec3
Caterina1996 <caterinamusi96@gmail.com>
2023-02-20 10:16:29 +0100

    update best model

--- scripts/runs_miki.py
+++ scripts/runs_miki.py
@@ -1,72 +1,92 @@
 
 import os
 
-
-
 # -------- BATCH  DA  SHAPE -------
 
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0003_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.003"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0003_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.003"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0009_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.009"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0009_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.009"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00033"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00033"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00011"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00011"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0001_d_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.001"
-os.system(command)
-command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0001_e_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.001"
-os.system(command)
-
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0003_d/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0003_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.003"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0003_e/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0003_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.003"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0009_d/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0009_d/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.009"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0009_e/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0009_e/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.009"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000033_d/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_d_da/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00033"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000033_e/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_e_da/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00033"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000011_d/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_d_da/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00011"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000011_e/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_e_da/ --data_path ../data/splits/cross2/a/ --shape 1024 --batch 3 --learning 0.00011"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0001_d_da/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0001_d_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.001"
 # os.system(command)
-# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0001_e_da/ -data_path ../data/splits/cross2/test/img --shape 1024"
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_0001_e_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.001"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_a_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00033"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_b_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00033"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000033_c_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00033"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_a_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00011"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_b_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00011"
+# os.system(command)
+# command = "python3 train.py --run_path ../runs/3/da_lr/1024_3_000011_c_da/ --data_path ../data/splits/cross2/a_da/ --shape 1024 --batch 3 --learning 0.00011"
 # os.system(command)
 
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0003_d/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0003_d/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0003_e/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0003_e/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0009_d/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0009_d/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0009_e/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0009_e/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_d/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000033_d/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_e/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000033_e/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000011_d/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000011_d/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000011_e/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_000011_e/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0001_d_da/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0001_d_da/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
-# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0001_e_da/ -mask_path ../data/splits/cross2/test/mask --shape 1024"
+# command = "python3 inference.py --run_path ../runs/3/da_lr/1024_3_0001_e_da/ --data_path ../data/splits/cross2/test/img --shape 1024"
 # os.system(command)
+
+
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0003_d/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0003_e/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0009_d/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_0009_e/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_d/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_e/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000011_d/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+# command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000011_e/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+# os.system(command)
+command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_a_da/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+os.system(command)
+command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_b_da/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+os.system(command)
+command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_c_da/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+os.system(command)
+command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_d_da/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+os.system(command)
+command = "python3 evaluation.py --run_path ../runs/3/da_lr/1024_3_000033_e_da/ --mask_path ../data/splits/cross2/test/mask --shape 1024"
+os.system(command)
+
+
+
 
 
 # # ------ K FOLD ------
